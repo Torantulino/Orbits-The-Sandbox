8fe58ef5a03ea8f0ee31197ecf2ce4d7c5e4ccbe
Torantulino <40276179@live.napier.ac.uk>
2019-09-19 17:20:05 +0100

    Adds 'Tools' file for custom classes etc. Adds fixed size queue.

---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedSizedQueue<T> : Queue<T>
+{
+    private readonly object syncObject = new object();
+
+    public int MaxSize { get; private set; }
+
+    public FixedSizedQueue(int maxSize)
+    {
+        MaxSize = maxSize;
+    }
+
+    public new void Enqueue(T obj)
+    {
+        base.Enqueue(obj);
+        lock (syncObject)
+        {
+            while (base.Count > MaxSize)
+            {
+                T outObj = base.Dequeue();
+            }
+        }
+    }
+
+    public void setMaxSize(int size)
+    {
+        MaxSize = size;
+    }
+}
