75b8941cb48578ebd343f5213429930b747857ec
Torantulino <40276179@live.napier.ac.uk>
2020-01-21 01:10:25 +0000

    Tweaks...

---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem[] ps = gameObject.GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem p in ps)
+        {
+            var main = p.main;
+            main.useUnscaledTime  = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/UnscaledParticleSystem.cs
+++ Assets/Scripts/UnscaledParticleSystem.cs
@@ -3,24 +3,16 @@
 public class UnscaledParticleSystem : MonoBehaviour
 {
     private ParticleSystem ps;
-    public GameObject tutorialCursor;
-
     void Awake()
     {
         ps = GetComponent<ParticleSystem>();
         var main = ps.main;
         main.useUnscaledTime = true;
-
     }
 
     /// This function is called when the object becomes enabled and active.
     void OnEnable()
     {
-        //Vector3 cursorPos = cam.ScreenToWorldPoint(Input.mousePosition);
-        //transform.position = new Vector3(cursorPos.x, cursorPos.y, transform.position.z);
-        //transform.LookAt(Camera.main.transform.position);
         ps.Play();
     }
-
-    
 }
