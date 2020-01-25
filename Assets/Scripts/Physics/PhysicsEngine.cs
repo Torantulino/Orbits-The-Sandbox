ab665f07b13db6a889edcb7a290e801f493f8d94
Torantulino <40276179@live.napier.ac.uk>
2020-01-25 20:46:17 +0000

    Adds Vector3 Benchmarks, will remove.

--- Assets/Scripts/Physics/PhysicsEngine.cs
+++ Assets/Scripts/Physics/PhysicsEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
 
@@ -17,6 +18,7 @@ public class PhysicsEngine : MonoBehaviour
     public HashSet<int> objectIDs = new HashSet<int>();
     public Dictionary<string, GameObject> particleEffects = new Dictionary<string, GameObject>();
     public Dictionary<int, ForceExerter> strongest_force = new Dictionary<int, ForceExerter>();   //Strongest force acting on the object ID=KEY this physics update
+    public static float PHYSICS_TIMESTEP = 0.04f;
 
     public struct ForceExerter
     {
@@ -67,6 +69,90 @@ public class PhysicsEngine : MonoBehaviour
         foreach (UnityEngine.Object obj in objs)
             particleEffects.Add(obj.name, (GameObject)obj);
 
+
+        Stopwatch Auto_Method = new Stopwatch();
+        Stopwatch Manual_Method = new Stopwatch();
+
+
+        // TESTING
+
+        // Inter-Object Vector Obtain
+        Vector3 position1 = new Vector3(123.321f, 456.65f, 234.0f);
+        Vector3 position2 = new Vector3(33.10f, 123.4f, 454.4f);
+        Vector3 dir1 = Vector3.one;
+        Vector3 dir2 = Vector3.zero;
+
+        Auto_Method.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            dir1 = position1 - position2;
+        }
+        Auto_Method.Stop();
+
+        Manual_Method.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            dir2 = new Vector3(position1.x - position2.x, position1.y - position2.y, position1.z - position2.z);
+        }
+        Manual_Method.Stop();
+
+        UnityEngine.Debug.Assert(dir1 == dir2);
+        UnityEngine.Debug.Log("<b>Auto <color=magenta>Vector Subtraction</color> Method time: </b>" + Auto_Method.Elapsed.TotalMilliseconds);
+        UnityEngine.Debug.Log("<b>Manual <color=magenta>Vector Subtraction</color> Method time: </b>" + Manual_Method.Elapsed.TotalMilliseconds);
+        //-----------------------
+
+        // Magnitude Calculation
+        Auto_Method.Reset();
+        Manual_Method.Reset();
+
+        float sqrMag1 = 1;
+        float sqrMag2 = 0;
+
+        Auto_Method.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            sqrMag1 = dir1.sqrMagnitude;
+        }
+        Auto_Method.Stop();
+
+        Manual_Method.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            sqrMag2 = dir1.x * dir1.x + dir1.y * dir1.y + dir1.z * dir1.z;
+        }
+        Manual_Method.Stop();
+
+        UnityEngine.Debug.Assert(sqrMag1 == sqrMag2);
+        UnityEngine.Debug.Log("<b>Auto <color=green>Squared Magnitude</color> Method time: </b>" + Auto_Method.Elapsed.TotalMilliseconds);
+        UnityEngine.Debug.Log("<b>Manual <color=green>Squared Magnitude</color> Method time: </b>" + Manual_Method.Elapsed.TotalMilliseconds);
+        //-----------------------
+
+        // Force Calculation
+        Auto_Method.Reset();
+        Manual_Method.Reset();
+        Vector3 force1 = Vector3.one;
+        Vector3 force2 = Vector3.forward;
+
+        Auto_Method.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            force1 = dir1.normalized;
+        }
+        Auto_Method.Stop();
+
+        Manual_Method.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            float mag =  Mathf.Sqrt(sqrMag1);
+            force2 = new Vector3(dir1.x / mag, dir1.y /mag, dir1.z / mag);
+        }
+        Manual_Method.Stop();
+
+        UnityEngine.Debug.Assert(force1 == force2, "force1:" + force1 + "force2:" + force2);
+        UnityEngine.Debug.Log("<b>Auto <color=purple>Force Calculation</color> Method time: </b>" + Auto_Method.Elapsed.TotalMilliseconds);
+        UnityEngine.Debug.Log("<b>Manual <color=green>Force Calculation</color> Method time: </b>" + Manual_Method.Elapsed.TotalMilliseconds);
+        //-----------------------
+
     }
 
     // Simulate
@@ -79,6 +165,7 @@ public class PhysicsEngine : MonoBehaviour
         {
             Vector3 force = CalculateGravitationalForce(objectPair);
             //Excert force on both objects, due to Newton's Third Law of motion
+
             objectPair.O2.rb.AddForce(force);
             objectPair.O1.rb.AddForce(force * -1.0f); //in opositite direction
         }
@@ -87,7 +174,7 @@ public class PhysicsEngine : MonoBehaviour
     public Vector3 CalculateGravitationalForce(PhysicsObjectPair pair)
     {
         //Obtain Direction Vector
-        Vector3 dir = pair.O1.rb.position - pair.O2.rb.position;
+        Vector3 dir = pair.O1.rb.position - pair.O2.rb.position;    //NOTE: This causes 2x Rigidbody AND 2x Collider Syncs 
 
         //Obtain Distance, return if 0
         float distSqr = dir.sqrMagnitude;
@@ -95,19 +182,20 @@ public class PhysicsEngine : MonoBehaviour
             return Vector3.zero;
 
         //Calculate Magnitude of force
-        float magnitude = G * (pair.O1.rb.mass * pair.O2.rb.mass) / distSqr;
+        float forceMagnitude = G * (pair.O1.rb.mass * pair.O2.rb.mass) / distSqr;
 
         // Compare to current largest force for both objects
         ForceExerter current_max;
         // 1
-        if (!strongest_force.TryGetValue(pair.O1.ID, out current_max) || magnitude > current_max.magnitude)
-            strongest_force[pair.O1.ID] = new ForceExerter(pair.O2.ID, magnitude);
+        if (!strongest_force.TryGetValue(pair.O1.ID, out current_max) || forceMagnitude > current_max.magnitude)
+            strongest_force[pair.O1.ID] = new ForceExerter(pair.O2.ID, forceMagnitude);
         // 2
-        if (!strongest_force.TryGetValue(pair.O2.ID, out current_max) || magnitude > current_max.magnitude)
-            strongest_force[pair.O2.ID] = new ForceExerter(pair.O1.ID, magnitude);
+        if (!strongest_force.TryGetValue(pair.O2.ID, out current_max) || forceMagnitude > current_max.magnitude)
+            strongest_force[pair.O2.ID] = new ForceExerter(pair.O1.ID, forceMagnitude);
 
         //Calculate force
-        Vector3 force = dir.normalized * magnitude;
+        Vector3 force = dir.normalized * forceMagnitude;
+
         return force;
     }
 
@@ -171,7 +259,7 @@ public class PhysicsEngine : MonoBehaviour
         if (scale >= 0.0f && scale <= 100.0f)
         {
             Time.timeScale = scale;
-            Time.fixedDeltaTime = scale * 0.02f;
+            Time.fixedDeltaTime = scale * PHYSICS_TIMESTEP;
         }
     }
 
@@ -182,7 +270,7 @@ public class PhysicsEngine : MonoBehaviour
         ammount = Mathf.Clamp(Time.timeScale + ammount, 0.0f, 100.0f) - Time.timeScale;
 
         Time.timeScale += ammount;
-        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        Time.fixedDeltaTime = Time.timeScale * PHYSICS_TIMESTEP;
     }
 }
 
