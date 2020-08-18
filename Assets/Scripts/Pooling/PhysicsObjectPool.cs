3bdff72efb386eefd7b0b70cc660e66fc7008028
Daniel Korsah <dkorsah15@aol.co.uk>
2020-08-18 14:43:17 +0100

    PhysicsObjectsPool with overriding function for return to pool. Incorporated defaults to reset physicsobjects.

---
+++ Assets/Scripts/Pooling/PhysicsObjectPool.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObjectPool : ObjectPool
+{
+
+    protected override void ReturnObjectToPool(PhysicsObject returningObject, PhysicsObjectDefaults defaults)
+    {
+        //set defaults
+        returningObject.rb.mass = defaults.mass;
+        returningObject.gameObject.transform.localScale = new Vector3(defaults.size, defaults.size, defaults.size);
+        returningObject.temperature = defaults.temperature;
+        returningObject.trailRenderer.Clear();
+
+        returningObject.gameObject.SetActive(false);
+        objects.Enqueue(returningObject.gameObject);
+    }
+
+}
