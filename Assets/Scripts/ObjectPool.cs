3ab8b9412f56bd2781e7b96f7ce8fa0b10a09d38
Daniel Korsah <dkorsah15@aol.co.uk>
2020-08-18 10:01:41 +0100

    -Make object pool a gameobject. -Remove generics as was can assume we're only pooling gameobjecs.

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int defaultSize = 50;
+    private Queue<GameObject> objects = new Queue<GameObject>();
+
+    //get object from pool, create one if drawing more than the pool contains
+    public GameObject Pop()
+    {
+        if (objects.Count == 0)
+        {
+            AddObjects(1);
+        }
+
+        return objects.Dequeue();
+    }
+
+    //add a number of objects to the pool
+    public void AddObjects(int newObjectNumber)
+    {
+        for (int i = 0; i < newObjectNumber; i++)
+        {
+            GameObject newObject = Instantiate<GameObject>(prefab);
+            newObject.gameObject.SetActive(false);
+            objects.Enqueue(newObject);
+        }
+    }
+
+    public void ReturnToPool(GameObject returningObject)
+    {
+        returningObject.gameObject.SetActive(false);
+        objects.Enqueue(returningObject);
+    }
+
+}
