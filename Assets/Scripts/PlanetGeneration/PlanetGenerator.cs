f14a327bf28cd3f6420e79ddf1ab27e5134c1ca0
Torantulino <40276179@live.napier.ac.uk>
2020-08-18 15:17:45 +0100

    Adds initial planet generation properties.

---
+++ Assets/Scripts/PlanetGeneration/PlanetGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    [Header("Base Settings")]
+    public float Radius;
+    public int IcosphereSubdivisions;
+    public Material PlanetMaterial;
+    public bool SmoothNormals;
+    public bool Rotate;
+    public float TurnSpeed;
+    public List<ColorSetting> Colours = new List<ColorSetting>();
+
+    [Header("Ocean:")]
+    public bool DrawShore;
+    public float minShoreWidth;
+    public float maxShoreWidth;
+
+    [Header("Continents:")]
+    public int MaxAmountOfContinents;
+    public float ContinentsMinSize;
+    public float ContinentsMaxSize;
+    public float MinLandExtrusionHeight;
+    public float MaxLandExtrusionHeight;
+
+    [Header("Mountains:")]
+    public float MaxAmountOfMountains;
+    public float MountainBaseSize;
+    public float MinMountainHeight;
+    public float MaxMountainHeight;
+
+    [Header("Bumpiness:")]
+    public float MinBumpFactor;
+    public float MaxBumpFactor;
+
+    private GameObject planetGameObject;
+    private MeshRenderer meshRenderer;
+    private MeshFilter meshFilter;
+
+}
