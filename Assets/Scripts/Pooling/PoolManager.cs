eb5b298e03259acf3bead06523eabd9f5bbf33b5
Daniel Korsah <dkorsah15@aol.co.uk>
2020-08-21 13:35:58 +0100

    Pool manager object with dictionary of possible pools to use.

---
+++ Assets/Scripts/Pooling/PoolManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public static Dictionary<string, ObjectPool> PoolDictionary = new Dictionary<string, ObjectPool>();
+
+    void Start()
+    {
+        PoolDictionary.Add("shards", GameObject.Find("ShardPool").GetComponent<ObjectPool>());
+        PoolDictionary.Add("contentPanels", GameObject.Find("EntityPanelPool").GetComponent<ObjectPool>());
+    }
+
+}
